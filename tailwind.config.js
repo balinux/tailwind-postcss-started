41bd51bdb09109564fd650eff37c9d61a5c7888f
balinux <ryojuniyantara@gmail.com>
2022-09-05 14:50:11 +0800

    udpate

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./pages/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      maxHeight: {
+        '128': '32rem',
+      }
+    },
   },
   plugins: [],
 }
